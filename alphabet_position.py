b4b123c6e4aff75df745511e3c091bc180419048
tanhapon0405 <tonydude1999@gmail.com>
2020-01-24 22:43:40 +0700

    Fitst commit

---
+++ alphabet_position.py
@@ -0,0 +1,13 @@
+def alphabet_position(sentence):
+    ans = ''
+    for letter in sentence:
+        if letter.isalpha() == True:
+            if letter.isupper() == True:
+                letter = letter.lower()
+                ans += str(ord(letter)-96)
+                ans += ' '
+            else:
+                ans += str(ord(letter)-96)
+                ans += ' '       
+    return ans
+
